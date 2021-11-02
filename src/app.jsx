133f580f46494b6b5554e9fabaf7b4f46fec5c01
“roselynn411” <roselynnfrempong@gmail.com>
2021-11-02 10:53:47 -0400

    updates to app.jsx file

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,69 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
+import TopSpot from './topspot';
 
-class App extends Component {
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topspots: []
+    };
+  }
+  componentWillMount() {
+    Axios 
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      .then(response => response.data)
+      .then(topspots => this.setState({ topspots })); 
+   }
+
+   fetch('https://codepen.io/merobertsjr/pen/WayqqB', options)
+   .then(data => {
+       if (!data.ok) {
+         throw Error(data.status);
+        }
+        return data.json();
+       }).then(update => {
+       console.log(update);
+       // {
+       //
+       title: 'A blog post by Kingsley',
+       //
+       body: 'Brilliant post on fetch API',
+       //
+       userId: 1,
+       //
+       id: 101
+       // };
+       }).catch(e => {
+       console.log(e);
+       });
+
+
+
+
   render() {
     return (
-      <div className='App'></div>
+      <div className='App'>
+        <div className='container'>
+          <div className='jumbotron jumbotron-fluid padding-top-4'>
+          
+            <h1 className='display-4 text-center'>San Diego Top Spots</h1>
+            <p className='lead text-center'>A list of the top 30 places to see in San Diego, California.</p>
+            
+          </div>
+          { this.state.topspots.map(topspot => (
+		        <TopSpot
+			        key={topspot.id}
+			        name={topspot.name}
+			        description={topspot.description}
+			        location={topspot.location} 
+		        />
+	))
+}     
+        </div>  
+      </div>
+
     );
   }
 }
---
+++ src/topspot.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default props => (
+    <div className='well'>
+	<h4>{props.name}</h4>
+	<p>{props.description}</p>
+
+    <a classname="btn btn-primary" href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}>Open in Google Maps</a>
+
+
+</div>
+);
