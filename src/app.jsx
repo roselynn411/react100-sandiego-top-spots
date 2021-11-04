120064d19c31d62a8c8af4d5c2e6759de11a3c40
“roselynn411” <roselynnfrempong@gmail.com>
2021-11-04 16:37:22 -0400

    second commit

--- src/app.jsx
+++ src/app.jsx
@@ -2,43 +2,43 @@ import Axios from 'axios';
 import React, { Component } from 'react';
 import TopSpot from './topspot';
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      topspots: []
-    };
-  }
-  componentWillMount() {
-    Axios 
-      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
-      .then(response => response.data)
-      .then(topspots => this.setState({ topspots })); 
-   }
-
+  //   this.state = {
+  //     topspots: []
+  //   };
+  // }
+  // componentWillMount() {l
+  //   Axios 
+  //     .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+  //     .then(response => response.data)
+  //     .then(topspots => this.setState({ topspots })); 
+  //  }
 
   render() {
-    return (
-      <div className='App'>
-        <div className='container'>
-          <div className='jumbotron jumbotron-fluid padding-top-4'>
+    console.log("hello world");
+     return ( <div className='App'>Testname</div>
+//       <div className='App'>
+//         <div className='container'>
+//           <div className='jumbotron jumbotron-fluid padding-top-4'>
           
-            <h1 className='display-4 text-center'>San Diego Top Spots</h1>
-            <p className='lead text-center'>A list of the top 30 places to see in San Diego, California.</p>
+//             <h1 className='display-4 text-center'>San Diego Top Spots</h1>
+//             <p className='lead text-center'>A list of the top 30 places to see in San Diego, California.</p>
             
-          </div>
-          { this.state.topspots.map(topspot => (
-		        <TopSpot
-			        key={topspot.id}
-			        name={topspot.name}
-			        description={topspot.description}
-			        location={topspot.location} 
-		        />
-	))
-}     
-        </div>  
-      </div>
+//           </div>
+//           { this.state.topspots.map(topspot => (
+// 		        <TopSpot
+// 			        key={topspot.id}
+// 			        name={topspot.name}
+// 			        description={topspot.description}
+// 			        location={topspot.location} 
+// 		        />
+// 	))
+// }     
+      //   </div>  
+      // </div>
 
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import App from './app';
+// import "bootstrap-icons/font/bootstrap-icons.css";
 
 render(<App />, document.getElementById('root'));
+// console.log(document.getElementbyId('root'));
